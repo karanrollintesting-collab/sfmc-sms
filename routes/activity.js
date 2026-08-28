f54c3ca32de8ce21f5622bb3700b99ac33b7c83d
karanrollintesting-collab <karanrollintesting@gmail.com>
2026-08-28 17:36:03 +1000

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -108,7 +108,7 @@ exports.execute = function (req, res) {
     client.messages 
           .create({ 
              body: body,
-                         messagingService: messagingService,
+                         messagingServiceSid: messagingService,
              to: to
            }) 
           .then(message => console.log(message.sid)) 
